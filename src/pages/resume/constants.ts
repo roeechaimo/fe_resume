ed3d5e3ef9be86dd1f49f2c70904ed33fcb89017
roeechaimo <roee84@gmail.com>
2024-12-29 14:43:07 +0200

    feat: add resume item

--- src/pages/resume/constants.ts
+++ src/pages/resume/constants.ts
@@ -1,8 +1,21 @@
 import { ResumeItem } from './types';
 
 export const RESUME_ITEM = {
+  CHECKMARKX: {
+    title: '2024-present Senior Front-end Engineer at ”CHECKMARKX".',
+    description: {
+      summary:
+        'Develop and maintain large-scale web applications, working with micro-frontends.',
+      stack: [
+        { text: 'HTML', image: './resume/icons8-html-50.png' },
+        { text: 'CSS', image: './resume/icons8-css-49.png' },
+        { text: 'React', image: './resume/icons8-react-js-100.png' },
+        { text: 'Go', image: './resume/icons8-nodejs-64.png' },
+      ],
+    },
+  } as ResumeItem,
   CLEAR: {
-    title: '2022-present Front-end web developer at ”CLEAR".',
+    title: '2022-2024 Front-end web developer at ”CLEAR".',
     description: {
       summary:
         'Developed and maintained web applications from scratch including the usage of monitoring, analytics, and alerting tools.',
